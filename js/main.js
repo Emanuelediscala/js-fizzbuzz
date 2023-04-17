a35d63d5b0849ee8bc1eb39c76f0b40f37ef1a7b
Emanuele Di Scala <manueldiscala@gmail.com>
2023-04-17 17:48:27 +0200

    feat:added condition of reminder 1 step html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-// for (let x= 0; x < 100; x+1) {
-//     console.log (x)
-// }
+
+let contenuto = document.getElementById{container}
+
+for (let x = 1; x <= 100; x++) {
+    console.log (x)
+    if (x % 15== 0) {
+    
+    }
+    if (x % 5 == 0) {
+        
+    }
+    if (x % 3== 0)
+}
